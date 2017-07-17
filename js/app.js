e24369de51e58f8e67c14d172afae178cb522054
feng241x <feng241x@qq.com>
2017-07-17 22:53:08 +0800

    02-展示列表功能完成

--- js/app.js
+++ js/app.js
@@ -5,6 +5,13 @@
 		.controller('TodoController',['$scope',TodoController])
 
 	function TodoController ($scope){
-
+		// 1、展示任务列表
+		var vm = $scope;
+		var todoList = [
+			{id:1,name:'抽烟',isCompleted:false},
+			{id:2,name:'喝酒',isCompleted:true},
+			{id:3,name:'烫头发',isCompleted:false},
+		]
+		vm.todoList = todoList;
 	}
 })(angular);
